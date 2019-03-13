414f97a4124a1e47b0c8e5a5795f65ea666d5713
Milena Malysheva <milenamalush@gmail.com>
2019-03-13 22:02:24 +0100

    WIP: partition queries

--- db/crud/partition_queries.py
+++ db/crud/partition_queries.py
@@ -1,7 +1,58 @@
-from input_data.temp_input_data import table_name
-
-
-def create_partition(connector):
-    connector.query("SELECT " + table_name + "('hypo_part_range', 'PARTITION BY RANGE(id)');")
-
-# TODO: remove created hypothetical partition
+from input_data.queries import queries
+#
+# # TODO: sort cluster ids for clusters_queries keys in alphabetical order
+# def create_partitions(connector, clusters_queries, master_table):
+#     clusters_check_conditions = {}
+#     for sub_table, cluster_queries in clusters_queries.items():
+#         subclusters_check_conditions = []
+#         for subcluster_queries in cluster_queries:
+#             subcluster_check_conditions = []
+#             for idx, query in enumerate(subcluster_queries):
+#                 subclusters_check_conditions.append(
+#                     queries[idx] if query else 'NOT ' + queries[idx]
+#                 )
+#             subclusters_check_conditions.append(subcluster_check_conditions)
+#         subclusters_check_conditions_str = ' OR '.join(subclusters_check_conditions)
+#         connector.query("CREATE TABLE {0} ({1}) INHERITS ({3});".format(
+#             master_table + '_' + sub_table, subclusters_check_conditions_str, master_table))
+#         connector.commit()
+#         clusters_check_conditions[sub_table] = subclusters_check_conditions_str
+#     return clusters_check_conditions
+#
+# # TODO: clusters_to_merge is tuple, put on the first place cluster with larger amount of rows
+# def merge_two_clusters(connector, clusters_check_conditions, clusters_to_merge, master_table):
+#     # step 1: create trigger
+#     clusters_check_conditions[clusters_to_merge[0]] = clusters_check_conditions[clusters_to_merge[0]] + ' OR ' +\
+#                                                       clusters_check_conditions[clusters_to_merge[1]]
+#     del clusters_check_conditions[clusters_to_merge[1]]
+#
+#     trigger_conditions = []
+#     for cluster_table, cluster_conditions in clusters_check_conditions.items():
+#         if not trigger_conditions:
+#             trigger_conditions.append("IF ({0}) THEN INSERT INTO {1} "
+#                                       .format(clusters_check_conditions, cluster_table))
+#         else:
+#             trigger_conditions.append("ELSIF ({0}) THEN INSERT INTO {1} "
+#                                       .format(clusters_check_conditions, cluster_table))
+#
+#     trigger_function = """
+#     CREATE OR REPLACE FUNCTION {0}_insert_trigger()\n
+#     RETURNS TRIGGER AS $$
+#     BEGIN
+#         {1}VALUES (NEW.*);
+#         ELSE
+#             RAISE EXCEPTION 'Date out of range.  Fix the {0}_insert_trigger() function!';
+#         END IF;
+#         RETURN NULL;
+#     END;
+#     $$
+#     LANGUAGE plpgsql;
+#     """.format(master_table, 'VALUES (NEW.*);\n'.join(trigger_conditions))
+#
+#     connector.query(trigger_function)
+#     connector.commit()
+#
+#     # step 2: copy data from the second cluster to merge to the first one
+#
+#     # step 3: delete old table and add new one
+#     # TODO: recreate clusters table back
--- input_data/queries.py
+++ input_data/queries.py
@@ -7,3 +7,11 @@ queries = [
     "l_commitdate < l_receiptdate",
     "l_orderkey = 1"
 ]
+
+# queries = [
+#     {'column': 'l_shipdate', 'from': "'1993-01-01'", 'to': "'1994-01-01'"},
+#     {'column': 'l_discount', 'from': '0.07', 'to': '0.09'},
+#     {'column': 'l_quantity', 'from': '7', 'to': '24'},
+#     {'column': 'l_orderkey', 'from': '1', 'to': '2540'},
+#     {'column': 'l_suppkey', 'from': '2000', 'to': '10000'},
+# ]
